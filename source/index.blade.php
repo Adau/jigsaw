a9597be671b590b25eaef94a2411a871d5286e22
Adrien <adrien.dau@gmail.com>
2018-08-16 15:35:13 +0200

    Ajuster la structure HTML

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,9 @@
 @extends('_layouts.master')
 
 @section('body')
-<div class="container">
-    <h1>Hello <i class="fas fa-globe"></i>!</h1>
-</div>
+<main>
+    <div class="container">
+        <h1>Hello <i class="fas fa-globe"></i>!</h1>
+    </div>
+</main>
 @endsection
